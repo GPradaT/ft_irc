78ed60eb0006e145e59da219aa1f88df10420665
Guillem Prada Torres <134978329+GPradaT@users.noreply.github.com>
2024-10-02 22:05:40 +0200

    Incluido destructor

--- srcs/IRCMessage.cpp
+++ srcs/IRCMessage.cpp
@@ -12,11 +12,11 @@ void  IRCMessage::setCommand(const std::string &command)
 
 IRCMessage::IRCMessage(const std::string &buffer)
 {
-	std::sßtringstream	new_buffer(buffer);
+	std::stringstream	new_buffer(buffer);
 	std::string	token;
 	while (new_buffer >> token)
 	{
 		std::cout << "Valor del token actual -> " << token << std::endl;
 	}
 }
-IRCMessage::~IRCMessage() {} 
+IRCMessage::~IRCMessage() {}
