fe082a1a9d78122794fb493ef13744c3088f1af1
Guillem Prada Torres <134978329+GPradaT@users.noreply.github.com>
2024-10-07 11:25:47 +0200

    generando validador de estructura de mensaje

--- Core/includes/IRCMessage.hpp
+++ Core/includes/IRCMessage.hpp
@@ -11,21 +11,26 @@ class	IRCMessage
 		std::string					_command;
 		std::string					_trailing;
 		std::vector<std::string>	_params;
-		std::string					_outputCommand;
+		bool						_isValid;
+		std::string					_rawMessage;
 
 	public:
 		IRCMessage(const std::string &buffer);
 		~IRCMessage();
 
-		void	setPrefix(const std::string &prefix);
-		void	setCommand(const std::string &command);
-		void	setParams(std::vector<std::string> &params);
-		void	setTrailing(std::stringstream &new_buffer, const std::string &trailing);
+		const std::string				&getPrefix() const;
+		const std::string				&getCommand() const;
+		const std::vector<std::string>	&getParams() const;
+		const std::string				&getTrailing() const;
 
-		std::string					&getPrefix() const;
-		std::string					getCommand() const;
-		std::vector<std::string>	getParams() const;
-		std::string					&getTrailing() const;
+		bool							isValid() const;
+		void							print() const;
 
-		void		print() const;
+	private:
+		void							parseMessage(const std::string &buffer);
+
+		bool							isValidCommand(const std::string &command);
+		bool							isValidPrefix(const std::string &prefix);
+		bool							isValidMiddleParam(const std::string &param);
+		bool							isValidTrailingParam(const std::string &param);
 };
