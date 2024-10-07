2859388de0201bd2eadc5152be82f337c5cde501
Guillem Prada Torres <134978329+GPradaT@users.noreply.github.com>
2024-10-07 11:25:47 +0200

    generando validador de estructura de mensaje

--- Core/includes/IRCMessage.hpp
+++ Core/includes/IRCMessage.hpp
@@ -7,30 +7,30 @@
 class	IRCMessage
 {
 	private:
-		unsigned short				_port;
 		std::string					_prefix;
 		std::string					_command;
 		std::string					_trailing;
 		std::vector<std::string>	_params;
-		std::string					_outputCommand;
+		bool						_isValid;
+		std::string					_rawMessage;
 
 	public:
 		IRCMessage(const std::string &buffer);
 		~IRCMessage();
 
-		void	setPrefix(const std::string &prefix);
-		void	setCommand(const std::string &command);
-		void	setParams(std::vector<std::string> &params);
-		void	setTrailing(std::stringstream &new_buffer, const std::string &trailing);
-		void	setPort(const std::string &port);
+		const std::string				&getPrefix() const;
+		const std::string				&getCommand() const;
+		const std::vector<std::string>	&getParams() const;
+		const std::string				&getTrailing() const;
 
-		static void	validInput(int argc, char *argv[]);
+		bool							isValid() const;
+		void							print() const;
 
+	private:
+		void							parseMessage(const std::string &buffer);
 
-		std::string					&getPrefix() const;
-		std::string					getCommand() const;
-		std::vector<std::string>	getParams() const;
-		std::string					&getTrailing() const;
-
-		void		print() const;
+		bool							isValidCommand(const std::string &command);
+		bool							isValidPrefix(const std::string &prefix);
+		bool							isValidMiddleParam(const std::string &param);
+		bool							isValidTrailingParam(const std::string &param);
 };
--- Core/srcs/IRCMessage.cpp
+++ Core/srcs/IRCMessage.cpp
@@ -1,52 +1,59 @@
 
 #include "../includes/IRCMessage.hpp"
 
-void	IRCMessage::setPrefix(const std::string &prefix)
-{
-	_prefix = prefix.substr(1);
-}
-
-void	IRCMessage::setCommand(const std::string &command)
-{
-	_command = command;
-}
-
-void	IRCMessage::setParams(std::vector<std::string> &params)
-{
-	_params = params;
-}
-
-void	IRCMessage::setTrailing(std::stringstream &new_buffer, const std::string &trailing)
-{
-	std::string	temp;
-	new_buffer >> temp;
-	_trailing = trailing.substr(1) + " " + temp;
-	while (new_buffer >> temp)
-		_trailing += " " + temp;
-}
 
 IRCMessage::IRCMessage(const std::string &buffer)
 {
-	std::stringstream	new_buffer(buffer);
-	std::string	token;
-	while (new_buffer >> token)
-	{
-		if (token[0] == ':')
-		{
-			setTrailing(new_buffer, token);
-			break;
-		}
-		_params.push_back(token);
-	}
+	parseMessage(buffer);
 }
+
 IRCMessage::~IRCMessage() {}
 
-std::string					IRCMessage::getCommand() const
+bool	IRCMessage::isValid() const
 {
-	return this->_command;
+	return _isValid;
 }
 
-std::vector<std::string>	IRCMessage::getParams() const
+bool	IRCMessage::isValidCommand(const std::string &command)
 {
-	return this->_params;
+	
 }
+
+
+//PART OF OLD CONSTRUCTOR
+	// std::stringstream	new_buffer(buffer);
+	// std::string	token;
+	// while (new_buffer >> token)
+	// {
+	// 	if (token[0] == ':')
+	// 	{
+	// 		setTrailing(new_buffer, token);
+	// 		break;
+	// 	}
+	// 	_params.push_back(token);
+	// }
+
+// void	IRCMessage::setPrefix(const std::string &prefix)
+// {
+// 	_prefix = prefix.substr(1);
+// }
+
+// void	IRCMessage::setCommand(const std::string &command)
+// {
+// 	_command = command;
+// }
+
+// void	IRCMesbsage::setParams(std::vector<std::string> &params)
+// {
+// 	_params = params;
+// }
+
+// void	IRCMessage::setTrailing(std::stringstream &new_buffer, const std::string &trailing)
+// {
+// 	std::string	temp;
+// 	new_buffer >> temp;
+// 	_trailing = trailing.substr(1) + " " + temp;
+// 	while (new_buffer >> temp)
+// 		_trailing += " " + temp;
+// }
+
